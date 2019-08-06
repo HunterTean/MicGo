e3a1a8ed5c68cad14bad087b9d1b7e76285a266e
Liu Hongtian <353231777@qq.com>
2019-08-06 14:39:10 +0800

    test

--- app/src/main/java/com/micgo/others/SnowActivity.java
+++ app/src/main/java/com/micgo/others/SnowActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.micgo.R;
+import com.micgo.utils.KTVLog;
 
 /**
  * Created by liuhongtian on 18/1/2.
@@ -26,6 +27,28 @@ public class SnowActivity extends AppCompatActivity {
     public static Intent buildIntent(Context context) {
         Intent intent = new Intent(context, SnowActivity.class);
         return intent;
+    }
+
+    public enum Singleton {
+        INSTANCE;
+
+        public void test() {
+            KTVLog.d("Singleton", "hello single instance");
+        }
+    }
+
+    public static class Singleton5 {
+        private final static class SingletonHolder {
+            private static final Singleton5 INSTANCE = new Singleton5();
+        }
+
+        private Singleton5() {
+
+        }
+
+        public static Singleton5 getInstance() {
+            return SingletonHolder.INSTANCE;
+        }
     }
 
 }
